e77fdfae95fb5919875b7e556271fee11836c960
Ajmain Nishu <ajmainnishu@gmail.com>
2021-10-04 20:13:21 +0600

    feat: Added readme file and website icon

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dance School
+
+## [Website Live link](https://american-singer.netlify.app/)
+## Here is the link: [https://american-singer.netlify.app/](https://american-singer.netlify.app/)
+
+
+## Usages Purpose
+- This is a dance school website.
+- It can be used to know the many kind of dance names, pictures, duration, price etc. of dance school.
+- Knowing this information will allow you to learn a lot about dance. You will get encouragement in this yourself. To do something by yourself.
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import footerLogo from '../../images/header_logo.png'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import footerLogo from "../../images/header_logo.png";
 
 const Footer = () => {
     return (
@@ -8,10 +8,17 @@ const Footer = () => {
             <div className="text-center navbar__bg">
                 <img className="py-4" src={footerLogo} alt="" />
                 <p className="fs-5">Copyright &copy; 2021</p>
-                <p className="fs-6">Designed By: <span className="fw-bold text-secondary">Ajmain Iqtidar Rahman</span></p>
-                <NavLink className="text-decoration-none btn btn-secondary mb-4" to="/contact">Contact Us</NavLink>
+                <p className="fs-6">
+                    Designed By:{" "}
+                    <span className="fw-bold text-secondary">Ajmain Iqtidar Rahman</span>
+                </p>
+                <NavLink
+                    className="text-decoration-none btn btn-secondary mb-4"
+                    to="/contact"
+                >
+                    Contact Us
+                </NavLink>
             </div>
-            
         </div>
     );
 };
